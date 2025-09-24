edb94668ac784c02dca4609311a445cc60a43405
Geun Woo Kim <rmsdn1955@gmail.com>
2025-09-24 18:27:33 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration for MiniGames High Score
+// This file is loaded by index.html to initialize Firebase and Firestore.
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyD6wNUwGTASqa-8XIuFwuXCFze1HguslBI",
+  authDomain: "minigames-highscore.firebaseapp.com",
+  projectId: "minigames-highscore",
+  storageBucket: "minigames-highscore.firebasestorage.app",
+  messagingSenderId: "691780466706",
+  appId: "1:691780466706:web:e4926e94cd8b52639100cb",
+  measurementId: "G-9D7YM9FJE7"
+};
